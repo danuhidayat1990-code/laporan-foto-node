fee269afde65275489301a4034165fbcc58d7bc2
danuhidayat1990-code <danuhidayat1990@gmail.com>
2025-09-18 19:18:54 +0700

    Add Cloudinary config

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ app.use(express.urlencoded({ extended: true }));
 
 // -------------------- Cloudinary Config --------------------
 cloudinary.config({
-  cloud_name: "YOUR_CLOUD_NAME",   // ganti sesuai akun Cloudinary
-  api_key: "YOUR_API_KEY",
-  api_secret: "YOUR_API_SECRET",
+  cloud_name: "ddzctmkri",   
+  api_key: "513619779369171",
+  api_secret: "WvqB1vsmRI1IBXFF8NqA9y6EYsM",
 });
 
 const storage = new CloudinaryStorage({
